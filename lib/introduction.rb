4220e3ff72df7cb906840ce8296d1e17e46b8526
Erika N. Fairley <efairley98@gmail.com>
2019-07-24 05:07:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction (name) 
-  puts "Hi, my name is # {Josh.\n}."
+  puts "Hi, my name is # {Josh.\n }."
 end 
 def introduction (name, language)
-  puts "Hi, my name is # {Josh.\n} and I am learning to program in # {Ruby}."
+  puts "Hi, my name is # {Josh.\n } and I am learning to program in # {Ruby}."
  end 
